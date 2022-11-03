b84bc0a6cd7bcef283d32655456841347a56b4a7
TL <dondondony38@gmail.com>
2022-11-03 16:04:43 +0200

    change width

--- lib/presentation/screens/login/login_screen.dart
+++ lib/presentation/screens/login/login_screen.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:gellary/presentation/screens/login/widgets/background_login_widget.dart';
 import 'package:gellary/presentation/screens/login/widgets/in_front_of_login_widget.dart';
 
+import '../../../core/utils/constant.dart';
+
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    width =MediaQuery.of(context).size.width;
     return Scaffold(
       body: SafeArea(
         child: Stack(
